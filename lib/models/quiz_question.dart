508effea7c2353d2ebd660750c689941f7154ff9
Peter Huynh <peter.huynh@DMR-8275.local>
2023-06-20 11:20:20 -0500

    added a shuffle question component

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+
 
 class QuizQuestion{
   
@@ -6,4 +6,13 @@ class QuizQuestion{
 
   final String text;
   final List<String> answers;
+
+
+  List<String> getShuffledAnswers(){
+    final shuffledList = List.of(answers);
+
+    shuffledList.shuffle();
+
+    return shuffledList;
+  }
 }
