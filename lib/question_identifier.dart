3d01134fcce12e8f6cc9b0394e8362180f0061db
Peter Huynh <trinh1234533@yahoo.com>
2023-06-21 13:52:07 -0500

    finished formatting and styling summary screen

--- lib/question_identifier.dart
+++ lib/question_identifier.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class QuestionIdentifier extends StatelessWidget {
-  const QuestionIdentifier({super.key, required this.questionIndex, required this.isCorrectAnswer});
+  const QuestionIdentifier(
+      {super.key, required this.questionIndex, required this.isCorrectAnswer});
 
   final int questionIndex;
   final bool isCorrectAnswer;
@@ -10,8 +11,22 @@ class QuestionIdentifier extends StatelessWidget {
   Widget build(BuildContext context) {
     final questionNumber = questionIndex + 1;
     return Container(
-      decoration: BoxDecoration(color: isCorrectAnswer ? const Color.fromARGB(0, 52, 11, 185) : const Color.fromARGB(0, 203, 91, 223)),
-      child: Text(questionNumber.toString(),)
-    );
+      alignment: Alignment.center,
+      width: 30,
+      height: 30,
+        decoration: BoxDecoration(
+          color: isCorrectAnswer
+              ? const Color.fromARGB(255, 49, 144, 212)
+              : const Color.fromARGB(255, 244, 134, 246),
+
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Text(
+          questionNumber.toString(),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 76, 11, 188),
+          )
+        ));
   }
 }
--- lib/summary_items.dart
+++ lib/summary_items.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/question_identifier.dart';
 
 class SummaryItems extends StatelessWidget {
@@ -13,23 +13,31 @@ class SummaryItems extends StatelessWidget {
     final isCorrectAnswer = itemData['user_answer'] == itemData['correct_answer'];
 
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(vertical: 8,),
       child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     QuestionIdentifier(questionIndex: itemData['question_index'] as int, isCorrectAnswer: isCorrectAnswer,),
 
-                    Text(
-                      ((itemData['question_index'] as int) + 1).toString(),
-                    ),
+                    // Text(
+                    //   ((itemData['question_index'] as int) + 1).toString(),
+                    // ),
+                    const SizedBox(width: 20),
                     Expanded(
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(itemData['question'] as String),
+                          Text(itemData['question'] as String, style: GoogleFonts.lato(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),),
                           const SizedBox(
                             height: 5,
                           ),
-                          Text(itemData['user_answer'] as String),
-                          Text(itemData['correct_answer'] as String),
+                          
+                          Text(itemData['user_answer'] as String, style: const TextStyle(color: Color.fromARGB(255, 210, 139, 223),),),
+                          Text(itemData['correct_answer'] as String, style: const TextStyle(color: Color.fromARGB(255, 88, 161, 221),),),
                         ],
                       ),
                     ),
